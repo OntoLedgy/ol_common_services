788c55e36f20ec327309530de24d241f12626d55
Mesbah Khan <khanm@ontoledgy.io>
2022-02-23 22:11:38 +0100

    first draft

--- code/services/identification_services/uuid_service/uuids.go
+++ code/services/identification_services/uuid_service/uuids.go
@@ -11,13 +11,13 @@ type UUIDs struct {
 
 func GetUUID(
 	uuid_type int,
-	seeding_string string) (
+	seeding_string ...string) (
 	*UUIDs,
 	error) {
 
 	var uuid_error error
 
-	generated_uuid := new(UUIDs)
+	generated_uuid := &UUIDs{}
 
 	generated_uuid.UUID = new(uuid.UUID)
 
@@ -31,12 +31,15 @@ func GetUUID(
 	case 2:
 		// Parsing UUIDs from string input
 		*generated_uuid.UUID, uuid_error =
-			uuid.FromString(seeding_string)
+			uuid.FromString(
+				seeding_string[0])
 
 		if uuid_error != nil {
 			fmt.Printf("Something went wrong: %s", uuid_error)
 			return nil, uuid_error
 		}
+
+	default:
 
 	}
 	return generated_uuid, nil
